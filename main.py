ab1a58d989399addcf8e207e225b13a6bd8d8b68
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-10-23 16:35:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import openai
+from src.gpt_functions import write_story
 
 st.header('Children book generator')
+
+if "story" not in st.session_state:
+  st.session_state.story = None
 
 category_list = ['Faith & Spirituality', 'Lessons & Values', 'Traditions & Celebrations', 'Love, Legacy & Legends']
 category_desc_map = {"Faith & Spirituality": "Moments that connect us to God, hope or quiet wonder.",
@@ -21,32 +25,47 @@ if reader_type == 'Individual reader':
   age = st.text_input('Age')
   gender = st.text_input('Gender')
   relationship = st.text_input('Relationship to main character')
+else:
+  age = ""
+  gender = ""
+  reslationship = ""
 elif reader_type == 'Other':
   other_reader_type = st.text_input('Specify reader type')
 
 
 st.subheader("Main character information")
-st.text_input("Full name")
+name = st.text_input("Full name")
 
 st.text("Date of birth")
 col1, col2, col3 = st.columns(3)
 with col1:
   year = st.number_input("Year")
 with col2:
-  year = st.number_input("Month")
+  month = st.number_input("Month")
 with col3:
-  year = st.number_input("Day")
+  day = st.number_input("Day")
   
 st.selectbox("Gender", ["Male", "Female", "Animal"])
 
 
 st.subheader("General story characteristics")
 
-st.text_input("When did the story take place")
-st.text_input("Where did the story take place?")
-st.text_input("Any specifics about the time or place that would help bring the story to life? (optional)")
-if reader_type == 'Individual reader':
-  st.text_input("Relationship of the main characyer to the main reader")
+time = st.text_input("When did the story take place")
+place = st.text_input("Where did the story take place?")
+extra_specs = st.text_input("Any specifics about the time or place that would help bring the story to life? (optional)")
 
-st.text_input("Why do you want this story remembered? (optional)")
-st.text_input("Anything else that should be in the story? Funny details, exact phrases, or how the ending should feel?” (short paragraph)")
+rememberance = st.text_input("Why do you want this story remembered? (optional)")
+small_specs = st.text_input("Anything else that should be in the story? Funny details, exact phrases, or how the ending should feel?” (short paragraph)")
+
+reader_info {"type": reader_type, "age": age, "gender": gender, "realtionship": relationship}
+main_character_info = {"name": name, "birthdate": f"{str(year)}-{str(month)}-{str(day)}"}
+story_info {"time": time, "place": place, "extra": extra_specs, "small": small_specs}
+cat_desc = f"{category}: {desc}"
+
+gen_button = st.button("Generate story")
+if gen_button:
+  story = write_story(cat_desc, reader_info, main_character_info, story_info) 
+  st.session_state.story = story
+  
+if st.session_state.story:
+  st.write(story)
