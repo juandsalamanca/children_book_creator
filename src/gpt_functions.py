100400ba44b34bd8ff30ad4650a9d8d12304ddd9
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-10-23 21:36:39 -0500

    Update gpt_functions.py

--- src/gpt_functions.py
+++ src/gpt_functions.py
@@ -6,9 +6,12 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 def write_story(story_type, reader_info, main_character_info, story_info):
   #client = OpenAI()
+
   user_prompt = f"""
   I need you to write a storyof the type {story_type} for the audience {reader_info["type"]}
   """
+  if reader_info["type"] == "Individual reader":
+    user_prompt += f"of age {reader_info["age"]}, {reader_info["gender"]}"
   messages = [{"role":"system", "content":"You are an expert at searching for Movie an TV official information guiding yourself by official sources when available."},
             {"role": "user", "content":user_prompt}]
 
